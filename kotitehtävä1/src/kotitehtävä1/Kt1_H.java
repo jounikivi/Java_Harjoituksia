730d5c2955c2c07d8c3135c0a0a157e060cc6ecd
jounikivi <jounikivipera@gmail.com>
2023-01-25 20:43:13 +0200

    Update Kt1_H.java

--- kotitehtävä1/src/kotitehtävä1/Kt1_H.java
+++ kotitehtävä1/src/kotitehtävä1/Kt1_H.java
@@ -1,5 +1,14 @@
 package kotitehtävä1;
 
 public class Kt1_H {
-
+	public static void main(String[] args) {
+		int a = 0;
+		int b = 6;
+		if((a == 0 || b == 6) && !(a == 0 && b == 6)) {
+		    System.out.println("A on nolla tai b on kuusi, mutta ei molemmat");
+		}
+		else {
+			System.out.println("A ei ole nolla tai b ei ole kuusi tai molemmat ovat nolla tai kuusi");
+		}
+	}
 }
