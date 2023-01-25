9e56be53ed3d8f267dd56520b903c08db6e18755
jounikivi <jounikivipera@gmail.com>
2023-01-25 20:16:24 +0200

    Update KT1_F.java

--- kotitehtävä1/src/kotitehtävä1/KT1_F.java
+++ kotitehtävä1/src/kotitehtävä1/KT1_F.java
@@ -1,5 +1,16 @@
 package kotitehtävä1;
 
 public class KT1_F {
-
+	public static void main(String[] args) {
+		int a = 33;
+		int b = 13;
+		int tulos = a * b;
+		
+		if(tulos > 0) {
+			System.out.println(a + " * " + b + " = " + tulos + " on positiivinen");
+		}
+		else {
+			System.out.println(a + " * " + b + " = " + tulos + " ei positiivinen");
+		}
+	}
 }
