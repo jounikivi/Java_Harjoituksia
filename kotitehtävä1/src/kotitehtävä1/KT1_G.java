09729cce0e836d1f99027c9248dab00d3d65ff6a
jounikivi <jounikivipera@gmail.com>
2023-01-25 20:27:46 +0200

    Update KT1_G.java

--- kotitehtävä1/src/kotitehtävä1/KT1_G.java
+++ kotitehtävä1/src/kotitehtävä1/KT1_G.java
@@ -1,5 +1,15 @@
 package kotitehtävä1;
 
 public class KT1_G {
-
+	public static void main(String[] args) {
+		int a = 55;
+		int b = 32;
+		
+		if (a > b && a < 95 && b < 40) {
+			System.out.println("a on suurempi kuin b ja a on pienempi kuin 100 ja b on pienempi kuin 50");
+		}
+		else {
+			System.out.println("a ei täytä ehtoja");
+		}
+	}
 }
