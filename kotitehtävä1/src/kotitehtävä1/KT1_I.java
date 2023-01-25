8b433cd8148515d853e790856a0894ea1b242a62
jounikivi <jounikivipera@gmail.com>
2023-01-25 20:51:16 +0200

    Update KT1_I.java

--- kotitehtävä1/src/kotitehtävä1/KT1_I.java
+++ kotitehtävä1/src/kotitehtävä1/KT1_I.java
@@ -1,5 +1,15 @@
 package kotiteht‰v‰1;
 
 public class KT1_I {
-
+	public static void main(String[] args) {
+		int a = 4;
+		int b = 4;
+		
+		if(a == b && (a % b) % 2 == 0 && a != 0 && b !=0) {
+			System.out.println("A ja B ovat yht‰ suuria ja osam‰‰r‰ a jaettuna b:ll‰ on jaollinen kahdella eik‰ kumpikaan \nluvuista ole nolla");
+		}
+		else {
+			System.out.println("a tai b ei t‰yt‰ ehtoja");
+		}
+	}
 }
