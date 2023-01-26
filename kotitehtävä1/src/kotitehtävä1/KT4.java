31f13cd08066c7c9be29a56a000971fce4b4231c
jounikivi <jounikivipera@gmail.com>
2023-01-26 17:54:14 +0200

    muutoksia

--- kotitehtävä1/src/kotitehtävä1/KT4.java
+++ kotitehtävä1/src/kotitehtävä1/KT4.java
@@ -1,5 +1,26 @@
 package kotitehtävä1;
 
-public class KT4 {
+import java.util.Scanner;
 
+public class KT4 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Anna eka luku");
+		int num1 = s.nextInt();
+		
+		System.out.println("Anna toka luku");
+		int num2 = s.nextInt();
+		
+		System.out.println("Anna kolmas");
+		int num3 = s.nextInt();
+		
+		int min = Math.min(Math.min(num1, num2), num3);
+		int max = Math.max(Math.max(num1, num2), num3);
+		
+		int mid = num1 + num2 + num3 - min - max;
+		
+		System.out.println("Luvut pienimmästä suurimpaan: " + min + " " + mid + " " + max);
+		
+		
+	}
 }
--- kotitehtävä1/src/kotitehtävä1/KT5.java
+++ kotitehtävä1/src/kotitehtävä1/KT5.java
@@ -1,5 +1,30 @@
 package kotitehtävä1;
 
-public class KT5 {
+import java.util.Scanner;
 
+public class KT5 {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		System.out.println("Anna eka luku");
+		int num1 = s.nextInt();
+		
+		System.out.println("Anna toka luku");
+		int num2 = s.nextInt();
+		
+		System.out.println("Anna kolmas luku");
+		int num3 = s.nextInt();
+		
+		System.out.print("Anna neljäs luku: ");
+        int num4 = s.nextInt();
+
+        int min = Math.min(Math.min(Math.min(num1, num2), num3), num4);
+        int max = Math.max(Math.max(Math.max(num1, num2), num3), num4);
+
+        int mid1 = num1 + num2 + num3 + num4 - min - max;
+
+        int mid2 = num1 + num2 + num3 + num4 - min - max - mid1;
+
+        System.out.println("Luvut pienimmästä suurimpaan: " + min + " " + mid1 + " " + mid2 + " " + max);
+    }
 }
+
