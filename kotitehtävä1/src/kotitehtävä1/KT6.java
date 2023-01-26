63f1ec060c73725e88855895a20a4f37295e9df1
jounikivi <jounikivipera@gmail.com>
2023-01-26 18:14:55 +0200

    Update KT6.java

--- kotitehtävä1/src/kotitehtävä1/KT6.java
+++ kotitehtävä1/src/kotitehtävä1/KT6.java
@@ -1,5 +1,25 @@
 package kotiteht‰v‰1;
 
-public class KT6 {
+import java.util.Scanner;
 
+public class KT6 {
+	public static void main(String[] args) {
+		Scanner lukija = new Scanner(System.in);
+		System.out.println("Anna luku 1-3 v‰list‰");
+		int num = lukija.nextInt();
+		
+		if(num ==1) {
+			System.out.println("yksi");
+		}
+		else if(num ==2) {
+			System.out.println("kaksi");
+		}
+		else if(num ==3 ) {
+			System.out.println("kolme");
+		} else {
+			System.out.println("V‰‰r‰ luku");
+		}
+		
+		
+	}
 }
