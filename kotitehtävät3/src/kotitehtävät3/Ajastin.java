f7bc852ad76094d4725b718857519535a0da8768
jounikivi <jounikivipera@gmail.com>
2023-04-06 13:08:00 +0300

    m

--- kotitehtävät3/src/kotitehtävät3/Ajastin.java
+++ kotitehtävät3/src/kotitehtävät3/Ajastin.java
@@ -6,78 +6,64 @@ public class Ajastin {
     private int minuutit;
     private int sekunnit;
 
-    // Konstruktori, joka asettaa ajastimen ajan
-    public Ajastin(int minuutit, int sekunnit) {
-        asetaAika(minuutit, sekunnit);
+    public Ajastin(int min, int sek) {
+        setAika(min, sek);
     }
 
-    // Metodi, jolla asetetaan ajastimen aika
-    public void asetaAika(int minuutit, int sekunnit) {
-        // Tarkistetaan, ettei aika ole negatiivinen
-        if (minuutit < 0) {
-            minuutit = 0;
+    public void setAika(int min, int sek) {
+        // Jos sekuntimäärä on minuutti tai enemmän, lisätään ylimenevät sekunnit minuutteihin
+        if (sek >= 60) {
+            min += sek / 60;
+            sek %= 60;
         }
-        if (sekunnit < 0) {
-            sekunnit = 0;
+        // Jos arvo on negatiivinen, tulee arvoksi nolla
+        if (min < 0 || sek < 0) {
+            min = 0;
+            sek = 0;
         }
-        // Tarkistetaan, ettei aika ylitä viittä minuuttia
-        if (minuutit > 5) {
-            minuutit = 5;
-            sekunnit = 0;
+        // Ajastimen maksimiaika on 5 min, jos aika täyttyy, tulee arvoksi nolla
+        if (min > 5 || (min == 5 && sek > 0)) {
+            min = 5;
+            sek = 0;
         }
-        // Muunnetaan ylimääräiset sekunnit minuuteiksi
-        if (sekunnit >= 60) {
-            minuutit += sekunnit / 60;
-            sekunnit %= 60;
-        }
-        // Asetetaan ajastimen aika
-        this.minuutit = minuutit;
-        this.sekunnit = sekunnit;
+        this.minuutit = min;
+        this.sekunnit = sek;
     }
 
-    // Metodi, joka palauttaa ajastimen minuutit
     public int getMinuutit() {
-        return minuutit;
+        return this.minuutit;
     }
 
-    // Metodi, joka palauttaa ajastimen sekunnit
     public int getSekunnit() {
-        return sekunnit;
+        return this.sekunnit;
     }
 
-    // Metodi, joka tulostaa ajastimen ajan muodossa mm:ss
     public void tulosta() {
-        System.out.printf("%d:%02d\n", minuutit, sekunnit);
+        System.out.printf("%d:%02d\n", this.minuutit, this.sekunnit);
     }
-}
 
-public class Main {
+    public void lisaaMinuutti() {
+        setAika(this.minuutit + 1, this.sekunnit);
+    }
+
+    public void vahennaSekunti() {
+        setAika(this.minuutit, this.sekunnit - 1);
+    }
+
     public static void main(String[] args) {
         Scanner lukija = new Scanner(System.in);
-
-        // Pyydetään käyttäjältä minuutit ja sekunnit
         System.out.print("Syötä minuutit: ");
-        int minuutit = lukija.nextInt();
+        int min = lukija.nextInt();
         System.out.print("Syötä sekunnit: ");
-        int sekunnit = lukija.nextInt();
+        int sek = lukija.nextInt();
+        Ajastin ajastin = new Ajastin(min, sek);
 
-        // Luodaan ajastin käyttäjän syöttämällä ajalla
-        Ajastin ajastin = new Ajastin(minuutit, sekunnit);
-
-        // Tarkistetaan joka sekunti, onko aika kulunut loppuun
-        while (ajastin.getMinuutit() > 0 || ajastin.getSekunnit() > 0) {
-            ajastin.tulosta();
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            // Vähennetään yksi sekunti ajasta
-            ajastin.asetaAika(ajastin.getMinuutit(), ajastin.getSekunnit() - 1);
-        }
-
-        // Ilmoitetaan, kun aika on kulunut loppuun
-        System.out.println("Aika loppui!");
+        ajastin.tulosta();
+        ajastin.lisaaMinuutti();
+        ajastin.tulosta();
+        ajastin.vahennaSekunti();
+        ajastin.tulosta();
     }
 }
+
 
