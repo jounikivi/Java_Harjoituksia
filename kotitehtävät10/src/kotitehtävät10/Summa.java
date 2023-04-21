3b21c3f50a14d17051e1d12fece33b44833643c9
jounikivi <jounikivipera@gmail.com>
2023-04-21 08:33:35 +0300

    muu

---
+++ kotitehtävät10/src/kotitehtävät10/Summa.java
@@ -0,0 +1,11 @@
+package kotitehtevät10;
+
+public class Summa {
+    public static void main(String[] args) {
+        int luku1 = Integer.parseInt(args[0]);
+        int luku2 = Integer.parseInt(args[1]);
+        int summa = luku1 + luku2;
+        System.out.println("Lukujen " + luku1 + " ja " + luku2 + " summa on " + summa);
+    }
+}
+
